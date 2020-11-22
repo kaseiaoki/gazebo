42cf47d73a9f8ebe8a2df2900a2203bdfa44b1c4
kaseiaoki <aoki070207@gmail.com>
2020-11-22 21:46:03 +0900

    とりあえず記事の表示

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,35 @@
-var WPAPI = require( 'wpapi' );
-var wp = new WPAPI({ endpoint: process.env.apiUrl });
+import Link from 'next/link'
+import WP from 'wpapi'
 
-export default function Home() {
+const wpClient = new WP({
+  endpoint: process.env.apiUrl
+})
+
+export default function Page({ id, title, content }) {
   return (
-    <div>
-      now URL is {process.env.apiUrl}
-    </div>
+    <>
+      <div>{title} id : {id}</div>
+      <div
+        dangerouslySetInnerHTML={{ __html: content }}
+      >
+      
+     </div>
+    </>
   )
 }
+
+export async function getStaticProps() {
+  const posts = await wpClient.posts()
+  console.log(posts)
+  const id = posts[1].id
+  const title =  posts[1].title.rendered
+  const content = posts[1].content.rendered
+  return {
+    props: {
+      id,
+      title,
+      content
+    },
+  }
+}
+
