ebd512b0325d6b577b6d6b795ec09583927a0e69
kaseiaoki <aoki070207@gmail.com>
2020-11-22 23:53:29 +0900

    リファクタリング

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,23 @@
-import Link from 'next/link'
 import WP from 'wpapi'
+import Link from 'next/link'
 
 const wpClient = new WP({
   endpoint: process.env.apiUrl
 })
+
+export async function getStaticProps() {
+  const posts = await wpClient.posts()
+  const id = posts[1].id
+  const title =  posts[1].title.rendered
+  const content = posts[1].content.rendered
+  return {
+    props: {
+      id,
+      title,
+      content
+    },
+  }
+}
 
 export default function Page({ id, title, content }) {
   return (
@@ -20,17 +34,5 @@ export default function Page({ id, title, content }) {
   )
 }
 
-export async function getStaticProps() {
-  const posts = await wpClient.posts()
-  const id = posts[1].id
-  const title =  posts[1].title.rendered
-  const content = posts[1].content.rendered
-  return {
-    props: {
-      id,
-      title,
-      content
-    },
-  }
-}
+
 
