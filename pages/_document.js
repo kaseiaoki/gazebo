13e65264f8b20ad46c2120cdbc9a82fcbde8c897
aucuba <aoki.11235813@ezweb.ne.jp>
2020-11-23 14:30:07 +0900

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,7 @@ export default class GazeboDocument extends Document {
     return (
       <html>
         <Head>
-          <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css"/>
+           {/* <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css"/> */}
         </Head>
         <body className="custom_class">
           <Main />
